b1ae9585819845a3f64daf50090046d9f212f557
Natesh Narain <nnaraindev@gmail.com>
2015-03-28 14:17:27 -0400

    create a filter function to hide panels that do not contain the specified tags

---
+++ js/filter.js
@@ -0,0 +1,31 @@
+
+
+function filter(tags)
+{
+
+	// example selector
+	//$(".panel").filter(":not([data-tags~='HCS12'], [data-tags~='html'])").hide();
+
+	// select all panels that do not contain the tags
+
+	// create the tag selector string by joining together multiple [data-tags~='tag'] selections
+	var filter_selector = ":not(";
+
+	var splitTags = tags.split(' ');
+	for(var i = 0; i < splitTags.length; i++)
+	{
+		filter_selector += "[data-tags~='" + splitTags[i] + "']";
+
+		// if not the last element, add a ',' to separate
+		if(i + 1 < splitTags.length)
+		{
+			filter_selector += ', ';
+		}
+	}
+
+	// close the selector
+	filter_selector += ')';
+
+	// hide elements
+	$(".panel").filter(filter_selector).hide();
+}
