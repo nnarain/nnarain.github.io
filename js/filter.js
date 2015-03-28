0ed6566e155e7f334a95c003425cf747ed7b3e72
Natesh Narain <nnaraindev@gmail.com>
2015-03-28 17:31:36 -0400

    animated hide and show

--- js/filter.js
+++ js/filter.js
@@ -6,45 +6,27 @@ function filter(tags)
 	// example selector
 	//$(".panel").filter(":not([data-tags~='HCS12'], [data-tags~='html'])").hide();
 
-	// select all panels that do not contain the tags
+	// select panels with the tags
 
 	// create the tag selector string by joining together multiple [data-tags~='tag'] selections
-	var filter_selector = ":not(";
+	var panelSelector = "";
 
 	var splitTags = tags.split(' ');
 	for(var i = 0; i < splitTags.length; i++)
 	{
-		filter_selector += "[data-tags~='" + splitTags[i] + "']";
+		panelSelector += "[data-tags~='" + splitTags[i] + "']";
 
 		// if not the last element, add a ',' to separate
 		if(i + 1 < splitTags.length)
 		{
-			filter_selector += ', ';
+			panelSelector += ', ';
 		}
 	}
-
-	// close the selector
-	filter_selector += ')';
 
 	// hide elements
-	$(".panel").filter(filter_selector).hide();
+	$(".panel").filter(panelSelector).show("slow");
+	$(".panel").filter(":not(" + panelSelector + ")").hide("slow");
 }
-
-$("#bnSearch").click(
-	function()
-	{
-		var tags = $("#etSearch").val();
-
-		if(tags == "")
-		{
-			$(".panel").show();
-		}
-		else
-		{
-			filter(tags);
-		}
-	}
-)
 
 $("#Search").submit(
 	function(event)
@@ -55,7 +37,7 @@ $("#Search").submit(
 
 		if(tags == "")
 		{
-			$(".panel").show();
+			$(".panel").show("slow");
 		}
 		else
 		{
