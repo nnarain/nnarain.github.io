ee6bd3b9fdf14d1daef4c96caa055c4309d2fe73
Natesh Narain <nnaraindev@gmail.com>
2015-11-08 13:16:43 -0500

    adjusted feed layout. doc feed.js

--- js/feed.js
+++ js/feed.js
@@ -7,29 +7,39 @@ function getGithubActivityFeed(username, callback)
         
         var commits = {};
         
+        // loop for each object (objects are named 0, 1, 2, 3, etc...)
         for(var i = 0; i < len; i++)
         {
+            // get the event
             var event = data["" + i];
             
+            // only consider push events
             if(event.type == "PushEvent")
             {
+                // get the repo name
                 var name = event.repo.name;
                 
+                // ... if the repo hasn't been added to the dictionary
                 if(commits[name] === undefined)
                 {
+                    // add repo to dictionary
                     commits[name] = {
                         "num": 0,
                         "url": "https://github.com/" + name
                     };
                 }
                 
+                // increment commit count
                 commits[name].num += event.payload.distinct_size;
             }
         }
         
+        // create the result object
         var activity = {};
+        // add commits
         activity["commits"] = commits;
         
+        // send back to caller
         callback(activity);
     });
 }
