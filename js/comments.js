075dbc6a6641e7477310eb5624b29b43161ca180
Natesh Narain <nnaraindev@gmail.com>
2015-11-23 19:10:34 -0500

    added javascript code to get issue comments

---
+++ js/comments.js
@@ -0,0 +1,48 @@
+
+function getPostComments(issueNumber, callback)
+{
+    // get the issue, specified by number, for the site page
+    $.get("https://api.github.com/repos/nnarain/nnarain.github.io/issues/" + issueNumber, function(issueData){
+//      console.log(issueData);
+        
+        // get the issue's comments url
+        var commentsUrl = issueData.comments_url;
+        
+        // if the issuse has comments
+        if(issueData.comments != 0)
+        {
+            // get the comments
+            $.get(commentsUrl, function(commentsData){
+             //   console.log(commentsData);
+                
+                var len = commentsData.length;
+                
+                var ret = {"comments":[]};
+                
+                // for every comment object
+                for(var i = 0; i < len; ++i)
+                {
+                    var data = commentsData["" + i];
+                    
+                    var comment = {};
+                    comment["body"] = data.body;
+                    comment["date"] = data.created_at;
+                    
+                    comment["user"] = {
+                        "name"       : data.user.login,
+                        "avatar_url" : data.user.avatar_url,
+                        "url"        : "https://github.com/" + data.user.login
+                    };
+                    
+                    ret.comments.push(comment);
+                }
+                
+                callback(ret);
+            });
+        }
+        else
+        {
+            callback({"length":0});   
+        }
+    });
+}
