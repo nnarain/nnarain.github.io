ba53b34c960c111d592aa8b67f7e1b85bc26ae77
Natesh Narain <nnaraindev@gmail.com>
2017-03-31 12:01:09 -0400

    page adjustments

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
 	$(document).ready(function(){
 		// added bootstrap the tables
 		$('table').each(function(){
-			$(this).addClass('striped bordered highlight');
+			$(this).addClass('striped bordered highlight black-text');
 		});
 
 		$(".sidemenu-trigger").sideNav();
