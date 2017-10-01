f195d8ac6bea73b0e315b768f7e901f86d56c5c8
Natesh Narain <nnaraindev@gmail.com>
2017-10-01 16:57:24 -0400

    Added table scss

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,5 @@
 (function($){
 	$(document).ready(function(){
-		// added bootstrap the tables
-		$('table').each(function(){
-			$(this).addClass('table-background striped bordered black-text');
-		});
 
-		$(".sidemenu-trigger").sideNav();
 	});
 })(jQuery);
